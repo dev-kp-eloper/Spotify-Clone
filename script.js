0ca20dd00c0bd85cdd1385a1e723848cc6d10aaa
dev-kp-eloper <99352163+dev-kp-eloper@users.noreply.github.com>
2023-08-02 21:54:13 +0530

    Update script.js
    Added new Songs..hahaha

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("Welcome to Spotify");
 
-// Initialize the Variables
+// Initialize the Variables here
 let songIndex = 0;
 let audioElement = new Audio('songs/1.mp3');
 let masterPlay = document.getElementById('masterPlay');
